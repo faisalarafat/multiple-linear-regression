257204e189cd92e7f58e26a66f5bd967b334dcfb
Faisal Arafat <faisalarafatakhand@gmail.com>
2019-06-04 05:12:43 +0600

    Initial commit

---
+++ multiple_linear_regression.py
@@ -0,0 +1,46 @@
+# Multiple Linear Regression Implimentation
+
+# Import the Libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Import the dataset
+df = pd.read_csv('50_Startups.csv')
+X = df.iloc[:, :-1].values
+y = df.iloc[:, 4].values
+
+#Encoding categorical data 
+# Encoding the Independant Variable
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X=LabelEncoder()
+X[:, 3] = labelencoder_X.fit_transform(X[:, 3])
+onehotencoder = OneHotEncoder(categorical_features = [3])
+X = onehotencoder.fit_transform(X).toarray()
+
+# Avoiding the Dumpy variable trap
+X = X[:, 1:]
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Featuring Scalling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)"""
+
+# Fitting Multiole Linerar Regression on the Training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# Predicting the test set results
+y_pred = regressor.predict(X_test)
+ 
+
+# Building the optimal model using Backward Elimination
+import statsmodels.formula.api as sm
+X = np.append(arr = np.ones((50, 1)).astype(int), values = X, axis = 1)
+X_opt = X[:, ]
